4f2ecb10aeafba4c71a8e88c28bf2e4370a2f683
lackhoa <vodangkhoa873@gmail.com>
2020-05-13 07:52:37 +0300

    Added a server for frontend

---
+++ frontend/index.js
@@ -0,0 +1,6 @@
+let express = require("express");
+let app = express();
+app.use(express.static("resources/public"))
+
+let port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Relay server listening on port ${port}`));
